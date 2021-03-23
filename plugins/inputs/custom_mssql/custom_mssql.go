0af08354c6c47c1436c14902d84660f98ef006b8
jongwan kim <snn77@nhn.com>
2021-03-23 09:10:30 +0900

    test

--- plugins/inputs/custom_mssql/custom_mssql.go
+++ plugins/inputs/custom_mssql/custom_mssql.go
@@ -14,10 +14,7 @@ import (
 
 // SQLServer struct
 type SQLServer struct {
-	Servers       []string `toml:"servers"`
-	HealthMetric  bool     `toml:"health_metric"`
-	queries       MapQuery
-	isInitialized bool
+	Servers []string `toml:"servers"`
 }
 
 // Query struct
@@ -28,73 +25,12 @@ type Query struct {
 	OrderedColumns []string
 }
 
-// MapQuery type
-type MapQuery map[string]Query
-
-// HealthMetric struct tracking the number of attempted vs successful connections for each connection string
-type HealthMetric struct {
-	AttemptedQueries  int
-	SuccessfulQueries int
-}
-
 const defaultServer = "Server=.;app name=telegraf;log=1;"
 
-const (
-	healthMetricName              = "sqlserver_telegraf_health"
-	healthMetricInstanceTag       = "sql_instance"
-	healthMetricDatabaseTag       = "database_name"
-	healthMetricAttemptedQueries  = "attempted_queries"
-	healthMetricSuccessfulQueries = "successful_queries"
-)
-
 const sampleConfig = `
-## Specify instances to monitor with a list of connection strings.
-## All connection parameters are optional.
-## By default, the host is localhost, listening on default port, TCP 1433.
-##   for Windows, the user is the currently running AD user (SSO).
-##   See https://github.com/denisenkom/go-mssqldb for detailed connection
-##   parameters, in particular, tls connections can be created like so:
-##   "encrypt=true;certificate=<cert>;hostNameInCertificate=<SqlServer host fqdn>"
 servers = [
   "Server=192.168.1.10;Port=1433;User Id=<user>;Password=<pw>;app name=telegraf;log=1;",
 ]
-
-## "database_type" enables a specific set of queries depending on the database type. If specified, it replaces azuredb = true/false and query_version = 2
-## In the config file, the sql server plugin section should be repeated each with a set of servers for a specific database_type.
-## Possible values for database_type are - "AzureSQLDB" or "AzureSQLManagedInstance" or "SQLServer"
-
-## Queries enabled by default for database_type = "AzureSQLDB" are - 
-## AzureSQLDBResourceStats, AzureSQLDBResourceGovernance, AzureSQLDBWaitStats, AzureSQLDBDatabaseIO, AzureSQLDBServerProperties, 
-## AzureSQLDBOsWaitstats, AzureSQLDBMemoryClerks, AzureSQLDBPerformanceCounters, AzureSQLDBRequests, AzureSQLDBSchedulers
-
-# database_type = "AzureSQLDB"
-
-## A list of queries to include. If not specified, all the above listed queries are used.
-# include_query = []
-
-## A list of queries to explicitly ignore.
-# exclude_query = []
-
-## Queries enabled by default for database_type = "AzureSQLManagedInstance" are - 
-## AzureSQLMIResourceStats, AzureSQLMIResourceGovernance, AzureSQLMIDatabaseIO, AzureSQLMIServerProperties, AzureSQLMIOsWaitstats, 
-## AzureSQLMIMemoryClerks, AzureSQLMIPerformanceCounters, AzureSQLMIRequests, AzureSQLMISchedulers
-
-# database_type = "AzureSQLManagedInstance"
-
-# include_query = []
-
-# exclude_query = []
-
-## Queries enabled by default for database_type = "SQLServer" are - 
-## SQLServerPerformanceCounters, SQLServerWaitStatsCategorized, SQLServerDatabaseIO, SQLServerProperties, SQLServerMemoryClerks, 
-## SQLServerSchedulers, SQLServerRequests, SQLServerVolumeSpace, SQLServerCpu
-
-database_type = "SQLServer"
-
-include_query = []
-
-## SQLServerAvailabilityReplicaStates and SQLServerDatabaseReplicaStates are optional queries and hence excluded here as default
-exclude_query = ["SQLServerAvailabilityReplicaStates", "SQLServerDatabaseReplicaStates"]
 `
 
 // SampleConfig return the sample configuration
@@ -111,53 +47,21 @@ type scanner interface {
 	Scan(dest ...interface{}) error
 }
 
-func initQueries(s *SQLServer) error {
-	s.queries = make(MapQuery)
-	queries := s.queries
-	log.Printf("I! [inputs.custom_mssql]")
-
-	queries["SQLServerLogBackupSize"] = Query{ScriptName: "SQLServerLogBackupSize", Script: sqlServerLogBackupSize, ResultByRow: false}
-	s.isInitialized = true
-
-	return nil
-}
-
 // Gather collect data from SQL Server
 func (s *SQLServer) Gather(acc telegraf.Accumulator) error {
-	if !s.isInitialized {
-		if err := initQueries(s); err != nil {
-			acc.AddError(err)
-			return err
-		}
-	}
-
 	var wg sync.WaitGroup
-	var mutex sync.Mutex
-	var healthMetrics = make(map[string]*HealthMetric)
+	query := Query{ScriptName: "SQLServerLogBackupSize-CustomMssql", Script: sqlServerLogBackupSize, ResultByRow: false}
 
-	for _, serv := range s.Servers {
-		for _, query := range s.queries {
-			wg.Add(1)
-			go func(serv string, query Query) {
-				defer wg.Done()
-				queryError := s.gatherServer(serv, query, acc)
-
-				if s.HealthMetric {
-					mutex.Lock()
-					s.gatherHealth(healthMetrics, serv, queryError)
-					mutex.Unlock()
-				}
-
-				acc.AddError(queryError)
-			}(serv, query)
-		}
+	for _, server := range s.Servers {
+		wg.Add(1)
+		go func(serv string, query Query) {
+			defer wg.Done()
+			queryError := s.gatherServer(serv, query, acc)
+			acc.AddError(queryError)
+		}(server, query)
 	}
 
 	wg.Wait()
-
-	if s.HealthMetric {
-		s.accHealth(healthMetrics, acc)
-	}
 
 	return nil
 }
@@ -235,39 +139,13 @@ func (s *SQLServer) accRow(query Query, acc telegraf.Accumulator, row scanner) e
 		// values
 		for header, val := range columnMap {
 			if _, ok := (*val).(string); !ok {
-				fields[header] = (*val)
+				fields[header] = *val
 			}
 		}
 		// add fields to Accumulator
 		acc.AddFields(measurement, fields, tags, time.Now())
 	}
 	return nil
-}
-
-// gatherHealth stores info about any query errors in the healthMetrics map
-func (s *SQLServer) gatherHealth(healthMetrics map[string]*HealthMetric, serv string, queryError error) {
-	if healthMetrics[serv] == nil {
-		healthMetrics[serv] = &HealthMetric{}
-	}
-
-	healthMetrics[serv].AttemptedQueries++
-	if queryError == nil {
-		healthMetrics[serv].SuccessfulQueries++
-	}
-}
-
-// accHealth accumulates the query health data contained within the healthMetrics map
-func (s *SQLServer) accHealth(healthMetrics map[string]*HealthMetric, acc telegraf.Accumulator) {
-	for connectionString, connectionStats := range healthMetrics {
-		sqlInstance, databaseName := getConnectionIdentifiers(connectionString)
-		tags := map[string]string{healthMetricInstanceTag: sqlInstance, healthMetricDatabaseTag: databaseName}
-		fields := map[string]interface{}{
-			healthMetricAttemptedQueries:  connectionStats.AttemptedQueries,
-			healthMetricSuccessfulQueries: connectionStats.SuccessfulQueries,
-		}
-
-		acc.AddFields(healthMetricName, fields, tags, time.Now())
-	}
 }
 
 func (s *SQLServer) Init() error {
